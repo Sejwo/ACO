226902b9a3f82fb4b7a056a8edbb96d79b3bd760
Sejwo <80993076+Sejwo@users.noreply.github.com>
2024-07-22 23:39:08 +0200

    preparing function for reading longlat values from excel

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,17 @@
-//Rn I'm checking how to commit it nicely to github through VSC
-
-/* very early test of whether anything works, pretty much moved into AcoModel struct
-fn main() {
-    let path = "src\\sgb128_dist.txt";
-    let mut model = AcoModel::new_from_file(path).unwrap();
-    model.set_number_of_iterations(10);
-    model.set_ant_count(2);
-    model.set_init_alpha(1.5);
-    model.set_init_beta(2.0);
-    model.set_decay(0.3);
-    model.set_pheromone_value(30.0);
-    println!("Starting model");
-    model.run_model();
-}
-  // test matrix
-    let distances = vec![
-        vec![0.0, 3.0, 4.0, 2.0, 7.0],
-        vec![3.0, 0.0, 4.0, 6.0, 3.0],
-        vec![4.0, 4.0, 0.0, 5.0, 8.0],
-        vec![2.0, 6.0, 5.0, 0.0, 6.0],
-        vec![7.0, 3.0, 8.0, 6.0, 0.0]
-    ];
-
-    let mut map = MapForAnts::new(distances);
-
-    // Create ants
-    let mut stagnation_counter = 0;
-    let num_ants = 10000;
-    let mut ants: Vec<Ant> = (0..num_ants).map(|_| Ant::new(map.cities.len(), 1.0, 1.0)).collect();
-    let mut continue_iterations = true;
-    // Simulate ants' movements
-    for i in 0..50 {
-        // Number of iterations
-        if !continue_iterations {
-            break;
-        }
-        for ant in &mut ants {
-            //println!("New ant! \n"); when exploring stagnation
-            ant.generate_path(&map);
-            //println!("\n");
-        }
-        map.update_pheromones(&ants, 0.2, 1.0);
-        for ant in &ants {
-            if ant.distance_traveled < map.best_distance {
-                stagnation_counter = 0;
-                map.best_distance = ant.distance_traveled;
-                map.best_path = ant.visited_cities.clone();
-            } else {
-                println!("So we are not gucci");
-                stagnation_counter += 1;
-                if stagnation_counter == 30000 {
-                    println!("Stagnacja osiągnięta przy {}-tej generacji ", i);
-                    continue_iterations = false;
-                    break;
-                }
-            }
-        }
-    }
-
-    // Visualization (for demonstration, print the pheromone matrix)
-    println!("Pheromone matrix:");
-    for row in map.pheromones.iter() {
-        for pheromone in row.iter() {
-            print!("{:.2} ", pheromone);
-        }
-        println!();
-    }
-    println!("Best path: {:?}", map.best_path);
-    println!("Best distance: {:.2}", map.best_distance);
-}
- */
-
-use rand::Rng;
-use rand::distributions::WeightedIndex;
 use rand::distributions::Distribution;
-use std::io::{ BufRead, BufReader };
-use std::fs::File;
+use rand::distributions::WeightedIndex;
+use rand::Rng;
 use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use office::{Excel, Range, DataType};
 use Aco::utils::usize_float_multiplication;
-//next step im considering is making a stack struct for the ants, 
+//next step im considering is making a stack struct for the ants,
 //since each ant will only use a single array of known size and type by the time of intialisation of the ant
 //with that essentially I'll be able to maybe speed up each operation around saving a path for each ant, now i could probably do it by just using arrays since all sizes are known
 //but truth be told based off this https://doc.rust-lang.org/book/ch04-01-what-is-ownership.html and this https://doc.rust-lang.org/book/ch03-02-data-types.html but I got kind of intimidated
 //what if I use the default stack? I understand stack as essentially an array right? So I push i32 of 1 on the stack, then I push another i32 of 2, if I want to access the 1st value do I have to pop the second?
-//22.07.24 ye, so essentially i can't use arrays since their size needs to be known at compile time and since all the vectors are dependant on the distances matrix it's Joever 
+//22.07.24 ye, so essentially i can't use arrays since their size needs to be known at compile time and since all the vectors are dependant on the distances matrix it's Joever
 //what I may do instead since I'm unsure if the stack can be even optimal is writing another parser for data which takes long/lats and creates a matrix based on them, or even a nice map
 pub struct Ant {
     current_city: usize,
@@ -101,7 +29,7 @@ impl Ant {
         let distance_traveled = 0.0;
         let visited_cities = vec![];
         let start_city = current_city;
-        let mut path_taken = vec![];
+        let path_taken = vec![];
         Self {
             current_city,
             distance_traveled,
@@ -146,7 +74,7 @@ impl Ant {
             }
         }
         let total_probability: f64 = row_probabilities.iter().sum();
-        if total_probability == 0.0{
+        if total_probability == 0.0 {
             return self.start_city;
         }
         let dist = WeightedIndex::new(&row_probabilities).expect("problem jakiś no");
@@ -223,10 +151,7 @@ impl AcoModel {
             if num_cities == 0 {
                 num_cities = parts.len();
             }
-            let row: Vec<f64> = parts
-                .iter()
-                .map(|&x| x.parse().unwrap())
-                .collect();
+            let row: Vec<f64> = parts.iter().map(|&x| x.parse().unwrap()).collect();
             distances.push(row);
         }
 
@@ -236,6 +161,15 @@ impl AcoModel {
 
         Ok(Self::new(distances))
     }
+    //fn new_from_excel(file_path: &str, sheet: Option<&str>) -> AcoModel{
+      //  sheet.unwrap_or("Sheet1").to_string();
+       // let mut workbook = Excel::open(file_path).unwrap(); 
+       // let r = workbook.worksheet_range(sheet);
+       // for row in r.rows() {
+        //    println!("row={:?}, row[0]={:?}", row, row[0]);
+       // }
+       // return Self::new(vec![vec![1.0,2.0,3.0]]);
+    //} //work in progress
     fn update_pheromones(&mut self, ants: &Vec<Ant>, average_distance: f64) {
         // Evaporation step
         for row in self.pheromones.iter_mut() {
@@ -250,7 +184,8 @@ impl AcoModel {
                 for window in ant.path_taken.windows(2) {
                     if let [from, to] = window {
                         if from != to {
-                            let pheromone_deposit = self.pheromone_value / self.distances[*from][*to];
+                            let pheromone_deposit =
+                                self.pheromone_value / self.distances[*from][*to];
                             self.pheromones[*from][*to] += pheromone_deposit;
                             self.pheromones[*to][*from] += pheromone_deposit;
                         }
@@ -261,39 +196,46 @@ impl AcoModel {
     }
     fn run_model(&mut self) {
         let mut iterations_without_improvement = 0;
-    
+
         for iteration in 0..self.number_of_iterations {
             // Regenerate ants each cycle
             let mut ants: Vec<Ant> = (0..self.ant_count)
                 .map(|_| Ant::new(self.cities.len(), self.final_alpha, self.final_beta))
                 .collect();
-    
+
             for ant in &mut ants {
                 ant.generate_path(self);
             }
-    
+
             let average_distance = AcoModel::calculate_average_distance(&ants);
-    
+
             // Update pheromones selectively
             self.update_pheromones(&ants, average_distance);
-    
+
             let mut improved = false;
             for ant in &ants {
                 if ant.distance_traveled < self.best_distance {
                     println!(
-                    "new best at {:?} \n 
+                        "new best at {:?} \n 
                     beating previous best at {:?} \n 
                     on iteration {} \n
                     with alpha of {} \n
                     beta of {} \n
                     and current pheromone matrix {:?}
-                    ", ant.distance_traveled, self.best_distance, iteration, self.final_alpha, self.final_beta, self.pheromones);
+                    ",
+                        ant.distance_traveled,
+                        self.best_distance,
+                        iteration,
+                        self.final_alpha,
+                        self.final_beta,
+                        self.pheromones
+                    );
                     self.best_distance = ant.distance_traveled;
                     self.best_path = ant.visited_cities.clone();
                     improved = true;
                 }
             }
-    
+
             if improved {
                 iterations_without_improvement = 0;
             } else {
